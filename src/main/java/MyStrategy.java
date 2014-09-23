73ba1287c3e755b9bf60511aa668c6a3e9b34d94
Sergey Skripko <lightgray@mail.ru>
2014-09-23 21:53:09 +0400

    added useless string

--- src/main/java/MyStrategy.java
+++ src/main/java/MyStrategy.java
@@ -12,6 +12,7 @@ public final class MyStrategy implements Strategy {
     public void move(Hockeyist self, World world, Game game, Move move) {
         try {
             new HockeyistAI(self).play(world, game, move);
+            System.out.println("Useless string");
         } catch (ReturnException e) {
         }
     }
